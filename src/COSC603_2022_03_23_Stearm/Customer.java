5b62471940d3ff261ad162984228ddd4c644f055
jahanseir <jahanseir0209@saskpolytech.ca>
2022-03-25 14:28:45 -0600

    Initial commit

---
+++ src/COSC603_2022_03_23_Stearm/Customer.java
@@ -0,0 +1,33 @@
+package COSC603_2022_03_23_Stearm;
+//01- Class
+public class Customer {
+    private int id;
+    private String name;
+    private Integer tier;
+
+    //////////////////////   CONSTRUCTOR
+    public Customer(int id, String name, Integer tier) {
+        this.id = id;
+        this.name = name;
+        this.tier = tier;
+    }
+    //////////////////////   SETTER   &  GETTER
+        public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getTier() {
+        return tier;
+    }
+    public void setTier(Integer tier) {
+        this.tier = tier;
+    }
+}
---
+++ src/COSC603_2022_03_23_Stearm/Order.java
@@ -0,0 +1,61 @@
+package COSC603_2022_03_23_Stearm;
+//02- Class
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Set;
+
+public class Order {
+    private int id;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private String status;
+    private Customer costomer;
+
+    Set<Product> products;
+
+    //////////////////////   CONSTRUCTOR
+    public Order(int id, LocalDate orderDate, LocalDate deliveryDate, String status, Customer costomer) {
+        this.id = id;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.status = status;
+        this.costomer = costomer;
+    }
+    //////////////////////   SETTER   &  GETTER
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Customer getCostomer() {
+        return costomer;
+    }
+    public void setCostomer(Customer costomer) {
+        this.costomer = costomer;
+    }
+    public Set<Product> getProducts() {
+        return products;
+    }
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}

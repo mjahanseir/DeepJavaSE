d827b3715c51e3836dbd26ba7e84f28aaa990621
mjahanseir <mjahanseir@gmail.com>
2022-04-01 11:27:31 -0600

    Multithreading and Parallel Programming

---
+++ src/Thread/JavaCup17_Thread_2.java
@@ -0,0 +1,22 @@
+package Thread;
+class Interrupting extends Thread {
+
+    public static void main(String[] a)
+            throws InterruptedException{
+        Interrupting t = new Interrupting();
+        t.start();
+        Thread.sleep(1000);
+        t.interrupt();
+    }
+
+    @Override
+    public synchronized void run() {
+        try {
+            wait();
+            System.out.println("After wait");
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("Resume");
+    }
+}

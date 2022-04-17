6892b11672dd0d81d8fd1cb4515e63aaacaec4ec
mjahanseir <mjahanseir@gmail.com>
2022-04-17 15:33:48 -0600

    JDBC delete

--- src/JDBC/JDBCimtiaz/JDBCcurd.java
+++ src/JDBC/JDBCimtiaz/JDBCcurd.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 public class JDBCcurd {
     public static void main(String[] args) {
         String url="jdbc:mysql://127.0.0.1:3306/employees_database";
+        int rowsAffected;
         try {
             //Establish Connection Object
             Connection conn = DriverManager.getConnection(url, "root", "Mo123456789");
@@ -16,13 +17,17 @@ public class JDBCcurd {
             Statement statement= conn.createStatement();
 
             //SEND query to DB -> EXECUTE the statement
-            statement.executeUpdate("insert into employees_tbl  " +
+//            rowsAffected =statement.executeUpdate("insert into employees_tbl " +
+//                    "(id, name, dept, salary) " +
+//                    "value (800, \"John\", \"Sales\", 5500);");
+
+            rowsAffected =statement.executeUpdate("insert into employees_tbl " +
                     "(id, name, dept, salary) " +
-                    "value (800, \"John\", \"Sales\", 5500);");
+                    "value (900, \"Robert\", \"Sales\", 5100);");
             //Process the result
-            System.out.println("Execute an Insert statement");
+            System.out.println("Execute an Insert statement - rows affected : " +rowsAffected);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Error while insertion");
         }
 
 

44ec7b185c8f5837bf910253a93ac9152511867f
mjahanseir <mjahanseir@gmail.com>
2022-04-18 22:57:52 -0600

    commit

--- src/Network/MultiFXServer.java
+++ src/Network/MultiFXServer.java
@@ -16,8 +16,6 @@ import java.net.SocketException;
 import java.sql.SQLOutput;
 import java.util.Date;
 
-
-
 public class MultiFXServer  extends Application {
     // Text area for displaying contents
     private TextArea ta = new TextArea();
@@ -37,20 +35,9 @@ public class MultiFXServer  extends Application {
                 // Create a server socket
                 ServerSocket serverSocket = new ServerSocket(8000);
                 /////////////////////////////////////////////////////////////////
-                /*
-                runLater. public static void runLater(Runnable runnable) Run the specified Runnable
-                on the JavaFX Application Thread at some unspecified time in the future.
-                This method,which may be called from any thread, will post the Runnable to an event
-                queue and then return immediately to the caller.
-                 */
                 Platform.runLater(() -> {
                     ta.appendText("Multi Server started at " + new Date() + '\n');
                 });
-
-//
-//                // Create data input and output streams
-//                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-//                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
 
                 while (true) {
 
@@ -77,20 +64,6 @@ public class MultiFXServer  extends Application {
 /////////////////////// ie. stream setup & processing to a new thread as a runnable task
 /////////////////////// new Thread (            ).start();
                     new Thread (new HandleAClient(socket)).start();
-
-//                    // Receive radius from the client
-//                    double radius = inputFromClient.readDouble();
-//
-//                    // Compute area
-//                    double area = radius * radius * Math.PI;
-//
-//                    // Send area back to the client
-//                    outputToClient.writeDouble(area);
-//                    ///////////////////////////////////////
-//                    Platform.runLater(() -> {
-//                        ta.appendText("Radius received from client: "+ radius + '\n');
-//                        ta.appendText("Area is: " + area + '\n');
-//                    });
                 }
             } catch (SocketException e) {
                 System.out.println("Client Disconnected");
@@ -99,8 +72,6 @@ public class MultiFXServer  extends Application {
             }
         }).start();
     }
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Define the thread class for handling new connection
 
--- src/Network/chapter33/MultiThreadServer.java
+++ src/Network/chapter33/MultiThreadServer.java
@@ -103,12 +103,5 @@ public class MultiThreadServer extends Application {
       }
     }
   }
-  
-  /**
-   * The main method is only needed for the IDE with limited
-   * JavaFX support. Not needed for running from the command line.
-   */
-  public static void main(String[] args) {
-    launch(args);
-  }
+
 }

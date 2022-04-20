9a434c8d76ac3a2879b2f6682092dfd023e40c79
mjahanseir <mjahanseir@gmail.com>
2022-04-20 11:33:52 -0600

    JDBC

---
+++ src/DBCallable.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+public class DBCallable {
+    public static void main(String[] args) {
+
+//        Step 1
+//        try (Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.75.128/sakila", "student", "letmein")) {
+//        } catch (SQLException sqle) {
+//            System.out.println(sqle.getMessage());
+//        }
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.75.128/sakila", "student", "letmein")) {
+
+            // use a CallableStatement object to run things like stored procedures
+            // the call in bracket will be {call procedure( ? , ? , ?  , ......)}
+            //many variables we need.
+            CallableStatement callable= connection.prepareCall( " { call movie_report( ? , ?) } " );
+
+
+            ///////////////////BINDING PARAMETERS
+            callable.setString(1,"OPUS ICE");
+            callable.registerOutParameter(2, Types.VARCHAR);
+            ///////////////////execute
+            callable.execute();
+
+            System.out.println(       callable.getString(2)         );
+
+
+
+        } catch (SQLException sqle) {
+            System.out.println(sqle.getMessage());
+        }
+        //call movie report("OPUS ICE",@myvar);
+
+
+    }
+}

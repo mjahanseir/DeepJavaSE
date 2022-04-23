1d9da79969b531e57dc8e177d5288957ca833cc4
mjahanseir <mjahanseir@gmail.com>
2022-04-23 17:28:35 -0600

    Synchronized with lock

--- src/Thread/Review/C4_Account_withLock.java
+++ src/Thread/Review/C4_Account_withLock.java
@@ -2,9 +2,12 @@ package Thread.Review;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class C4_Account_withLock {
-private static Account account= new Account();
+
+    private static Account account= new Account();
     public static void main(String[] args) {
 
         ExecutorService executor= Executors.newCachedThreadPool();
@@ -27,12 +30,14 @@ private static Account account= new Account();
     ///////////////////////////////////////////////////////////
     private static class Account {
         private int balance=0;
+        private static Lock lock = new ReentrantLock();
 
         public int getBalance() {
             return balance;
         }
 
-        public /* synchronized */ void deposite(int amount) {
+        public void deposite(int amount) {
+            lock.lock();
             int newBalance = balance + amount;
             try {
                 Thread.sleep(5);
@@ -40,6 +45,13 @@ private static Account account= new Account();
                 e.printStackTrace();
             }
             balance= newBalance;
+            lock.unlock();
         }
+        /*
+         * Note:
+         * How do you create a lock object? How do you acquire a lock and release a lock?
+         * To create a lock, use the new ReentrantLock().
+         * To acquire the lock, invoke its lock() method and to release it, invoke its unlock() method.
+         */
     }//End of Account
 }//End of Class

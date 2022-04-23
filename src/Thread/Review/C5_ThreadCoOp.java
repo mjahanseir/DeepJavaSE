cebdafcbf21745107039bd7c457f331bd5dd9102
mjahanseir <mjahanseir@gmail.com>
2022-04-23 17:56:12 -0600

    Thread Cooperation setting up

--- src/Thread/Review/C5_ThreadCoOp.java
+++ src/Thread/Review/C5_ThreadCoOp.java
@@ -1,4 +1,32 @@
 package Thread.Review;
 
 public class C5_ThreadCoOp {
-}
+    private static Account account= new Account();
+
+    public static void main(String[] args) {
+
+    }//END OF MAIN
+    public static class Deposit implements Runnable{
+        @Override
+        public void run() {
+
+        }
+    }//END of Deposit
+    public static class Withdraw implements Runnable{
+
+        @Override
+        public void run() {
+
+        }
+    }//END OF Withdraw
+    private static class Account{
+
+        public void deposit(int amount){
+
+        }
+        public void withdraw(int amount){
+
+        }
+    }//END OF Account
+
+}// END OF CLASS

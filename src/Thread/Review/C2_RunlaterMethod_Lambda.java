4098b4511219deb7235fdf8612d4472f51b4a473
mjahanseir <mjahanseir@gmail.com>
2022-04-23 12:28:36 -0600

    Platform RUN LATER with Lambda Exp.

---
+++ src/Thread/Review/C2_RunlaterMethod_Lambda.java
@@ -0,0 +1,37 @@
+package Thread.Review;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class C2_RunlaterMethod_Lambda extends Application {
+    private String txt="";
+
+    @Override
+    public void start(Stage stage){
+
+        StackPane sp= new StackPane();
+        Label lbl=new Label("Hi");
+        sp.getChildren().add(lbl);
+
+        new Thread(()->{
+                try {
+                    while (true) {
+                        if (lbl.getText().trim().length() == 0)  txt = "Bye";
+                        else  txt = "";
+                        Platform.runLater(()->lbl.setText(txt));
+                        Thread.sleep(200);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+        }).start();
+        Scene scene= new Scene(sp , 200,50);
+        stage.setScene(scene);
+        stage.show();
+    }
+}

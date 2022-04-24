d15fce533ca26935dcb665d63a513e74085f5d4e
mjahanseir <mjahanseir@gmail.com>
2022-04-24 15:47:56 -0600

    more threading

--- src/Thread/Review/C0328/ThreadsDemo/MyThreadSubClass.java
+++ src/Thread/Review/C0328/ThreadsDemo/MyThreadSubClass.java
@@ -4,6 +4,7 @@ import static Thread.Review.C0328.ThreadsDemo.TextColor.*;
 public class MyThreadSubClass extends Thread{
     @Override
     public  void run(){
-        System.out.println(BLUE+"Output from " + currentThread().getName()); // thread subclass
+        System.out.println(BLUE+"Output from " + currentThread().getName());
+        // thread subclass
     }
 }

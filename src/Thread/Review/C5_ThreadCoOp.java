6aba747104ecb599eb0b503feef1a9f9388490e7
mjahanseir <mjahanseir@gmail.com>
2022-04-23 18:21:31 -0600

    Thread Cooperation

--- src/Thread/Review/C5_ThreadCoOp.java
+++ src/Thread/Review/C5_ThreadCoOp.java
@@ -2,6 +2,7 @@ package Thread.Review;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -29,14 +30,45 @@ public class C5_ThreadCoOp {
         }
     }//END OF Withdraw
     private static class Account{
-        private Lock lock= new ReentrantLock();
+        private static Lock lock= new ReentrantLock(); //A lock is created
+        // The deposit task acquires the lock in line 76
+//and signals all waiting threads (line 83) for the newDeposit condition after a new deposit
+//is made.
+        private static Condition newDeposit = lock.newCondition(); // A condition named newDeposit on the lock is created
+        // A condition is bound to a lock. Before waiting or signaling the condition,
+        // a thread must first acquire the lock for the condition.
+        private int balance=0;
 
-        public void deposit(int amount){
-
+        public int getBalance() {
+            return balance;
         }
-        public void withdraw(int amount){
 
+        public void withdraw(int amount){
+            //The withdraw task acquires the lock , waits for the newDeposit condition
+            // when there is not a sufficient amount to withdraw, and releases the lock.
+            lock.lock();
+            try {
+                while (balance<amount){
+                    System.out.println(" 	*********	  WAIT  	*********	");
+                    newDeposit.await();
+                }
+                balance-=amount;
+                System.out.println("\t\t\t   Withdraw  "+ amount + "\t\t "+ getBalance());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+        public void deposit(int amount){
+            lock.lock();
+            try {
+                balance+=amount;
+                System.out.println("Deposit  " + amount + "\t\t\t\t"+ getBalance());
+                newDeposit.signalAll();
+            }finally {
+                lock.unlock();
+            }
         }
     }//END OF Account
-
 }// END OF CLASS

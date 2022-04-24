7a2bf6970a306e12b5a93095b019abfafe16db16
mjahanseir <mjahanseir@gmail.com>
2022-04-23 20:03:46 -0600

    Producer and Consumer Approach figuring writing staff out

--- src/Thread/Review/ProducerConsumerProgram.java
+++ src/Thread/Review/ProducerConsumerProgram.java
@@ -1,5 +1,10 @@
 package Thread.Review;
 
+import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumerProgram {
 
     public static void main(String[] args) {
@@ -21,7 +26,28 @@ public class ProducerConsumerProgram {
 
     /** n inner class for buffer */
     private static class Buffer{
-        public void write(int value){}
+        private static final int CAPACITY = 1; // buffer size
+        private LinkedList<Integer> queue = new LinkedList<>();
+
+        private static Lock lock= new ReentrantLock();
+        private static Condition noEmpty= lock.newCondition();
+        private static Condition noFull= lock.newCondition();
+
+        public void write(int value){
+            lock.lock(); // Acquire the lock
+            try {
+                while (queue.size() == CAPACITY) {
+                    System.out.println("Wait for notFull condition");
+                    noFull.await();
+                }
+                queue.offer(value);
+                noEmpty.signal();
+            }catch (InterruptedException ex){
+                ex.printStackTrace();
+            }finally {
+                lock.unlock();
+            }
+        }
         public int read(){}
     }// END OF BUFFER
 

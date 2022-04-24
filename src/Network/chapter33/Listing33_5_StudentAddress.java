258de7387d9d0ef856165b24da72d3b4250889da
mjahanseir <mjahanseir@gmail.com>
2022-04-24 13:26:21 -0600

    Object input and out put stream setting client

--- src/Network/chapter33/Listing33_5_StudentAddress.java
+++ src/Network/chapter33/Listing33_5_StudentAddress.java
@@ -1,14 +1,14 @@
 package Network.chapter33;
 
-public class StudentAddress implements java.io.Serializable {
+public class Listing33_5_StudentAddress implements java.io.Serializable {
   private String name;
   private String street;
   private String city;
   private String state;
   private String zip;
 
-  public StudentAddress(String name, String street, String city,
-    String state, String zip) {
+  public Listing33_5_StudentAddress(String name, String street, String city,
+                                    String state, String zip) {
     this.name = name;
     this.street = street;
     this.city = city;

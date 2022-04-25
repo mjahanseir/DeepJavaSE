0e8e9186c4cf9df05918ad770d64dd4033d04fcb
mjahanseir <mjahanseir@gmail.com>
2022-04-25 09:20:37 -0600

    JDBC

--- src/JDBC/Review/C8_DBtester_Parameterized.java
+++ src/JDBC/Review/C8_DBtester_Parameterized.java
@@ -8,25 +8,16 @@ public class C8_DBtester_Parameterized {
         try(Connection connection= DriverManager.getConnection(
                 "jdbc:mysql://localhost/sakila", "root", "Mo123456789")) {
             //2
-            PreparedStatement ps = connection.prepareStatement(
-                    "select * from actor where actor_id=?");
-            PreparedStatement ps2 = connection.prepareStatement(
-                    "select * from actor where actor_id=? and first_name like ?");
-            //3
-            int id=201;
-            String name= "DAVE";
+            CallableStatement callableStatement= connection.prepareCall("{call movie_report(?,?)}");
+             //3
+            callableStatement.setString(1,"OPUS ICE");
             //4
-            ps2.setInt(1,id);
-            ps2.setString(2,name);
+            callableStatement.registerOutParameter(2,Types.VARCHAR);
             //5
-            ResultSet resultSet=ps2.executeQuery();
+callableStatement.execute();
             //6
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1) + "\t"+
-                        resultSet.getString(2) + "\t"+
-                        resultSet.getString(3) + "\t"+
-                        resultSet.getString(4) + "\t");
-            }
+            System.out.println(  callableStatement.getString(2)  );
+
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
